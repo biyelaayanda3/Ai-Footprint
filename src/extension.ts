f5184eb540c77142338f94447fc1220b0e262210
biyelaayanda3 <biyelaayanda3@gmail.com>
2026-06-21 00:51:11 +0200

    Add a new test case for this extention

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,40 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	console.log('AI Footprint is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "ai-footprint" is now active!');
+	// This fires every time text changes in the editor
+	const textChangeListener = vscode.workspace.onDidChangeTextDocument((event) => {
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('ai-footprint.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from ai-footprint!');
+		// Loop through every change in this event
+		// (there can be multiple, e.g. multi-cursor editing)
+		for (const change of event.contentChanges) {
+
+			const textAdded = change.text;
+			const linesAdded = textAdded.split('\n').length - 1;
+			const timestamp = Date.now();
+
+			// Log every change so we can see what's happening
+			console.log('--- Change Detected ---');
+			console.log('Lines added:', linesAdded);
+			console.log('Characters added:', textAdded.length);
+			console.log('Timestamp:', timestamp);
+
+			// PASTE DETECTION
+			// A paste = lots of lines appearing in a single change event
+			const PASTE_THRESHOLD = 5; // we'll make this configurable later
+
+			if (linesAdded >= PASTE_THRESHOLD) {
+				console.log('🚨 PASTE DETECTED');
+				vscode.window.showWarningMessage(
+					`AI Footprint: Large paste detected (${linesAdded} lines). Make sure you understand this code!`
+				);
+			}
+		}
 	});
 
-	context.subscriptions.push(disposable);
+	// Always push listeners to subscriptions for cleanup
+	context.subscriptions.push(textChangeListener);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
